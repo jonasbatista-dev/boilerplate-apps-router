af14459054330f45aae461335a01299dfa1449b4
jonas.batista <jonas.batista@spotmetrics.com>
2025-04-27 02:51:08 -0300

    config husky and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+// .lintstagedrc.js
+module.exports = {
+  '*.{js,jsx,ts,tsx}': (filenames) => [
+    `prettier --write ${filenames.map((f) => `"${f}"`).join(' ')}`,
+    `npm run lint --fix . ${filenames.map((f) => `--file "${f}"`).join(' ')}`,
+  ],
+};
